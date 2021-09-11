9ca4d8090337749459d4f067847397de056a5a56
Srivatsan Sridhar <vatsakrish@gmail.com>
2021-09-11 14:58:32 -0400

    audio js trigger

--- assets/js/main.js
+++ assets/js/main.js
@@ -97,6 +97,12 @@
     })
 
 
+    window.addEventListener("DOMContentLoaded", event => {
+        const audio = document.querySelector("audio");
+        audio.volume = 0.8;
+        audio.play();
+    });
+
     /**
      * Audio Stop
      */
